265710aab88de5969a8591188e619b6997b0bdf3
Sayan Ghorui <sayanghorui611@gmail.com>
2025-07-29 08:31:56 +0530

    Time: 13 ms (88.99%), Space: 58.8 MB (31.26%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length == 1) return false; 
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums) {
+            if(set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
