c40bb5e16292c71f8dd7d90bb02b1c517ce7eacc
Sayan Ghorui <sayanghorui611@gmail.com>
2025-08-11 09:34:56 +0530

    Time: 4 ms (91.4%), Space: 89.3 MB (45.16%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
@@ -0,0 +1,26 @@
+class Solution {
+    private int mod = (int) 1e9+7;
+    public int[] productQueries(int n, int[][] queries) {
+        List<Integer> powList = new ArrayList<>();
+        while (n > 0) {
+            int lowBit = n & -n;
+            powList.add(lowBit);
+            n ^= lowBit;
+        }
+        int size = powList.size();
+        int[][] prodTable = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            prodTable[row][row] = powList.get(row);
+            for (int col = row + 1; col < size; col++) {
+                prodTable[row][col] = (int)((long)prodTable[row][col - 1] * powList.get(col) % mod);
+            }
+        }
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int p = queries[i][0];
+            int q = queries[i][1];
+            result[i] = prodTable[p][q];
+        }
+        return result;
+    }
+}
